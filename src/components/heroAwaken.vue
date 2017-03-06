a0db7ef23ce8e37793882b719e06ea298bec00cf
xing <18331097123@163.com>
2017-03-06 17:35:05 +0800

    feat: 添加式神觉醒材料和技能模块

---
+++ src/components/heroAwaken.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="heroAwaken">
+        觉醒材料
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'heroAwaken'
+    };
+</script>
+
+<style lang="less">
+@import '../util.less';
+    .heroAwaken {
+        .px2rem(font-size, 25);
+        .px2rem(padding, 15);
+    }
+</style>
---
+++ src/components/heroSkills.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="heroSkills">
+        <div class="skill">技能一</div>
+        <div class="skill">技能二</div>
+        <div class="skill">技能三</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'heroSkills'
+    };
+</script>
+
+<style lang="less">
+@import '../util.less';
+.heroSkills{
+    .px2rem(font-size, 25);
+    .px2rem(padding, 15);
+}
+</style>
