8a050441e42171be564542a0c735ddbc71ba4751
王亚星 <funnycoderstar@gmail.com>
2017-08-05 14:52:30 +0800

    feat: 添加搜索不存在的式神显示的信息

--- src/components/searchHero.vue
+++ src/components/searchHero.vue
@@ -2,8 +2,8 @@
     <div class="heroList">
         <ComHead :pageTitle="title">
         </ComHead>
-        <div class="wrap-heroList fix-float searchHero">
-            <ul class="heroItem" v-for="item in herosInfo" @click="showHeroInfo(item.name)">
+        <div class="wrap-heroList fix-float searchHero" v-if="herosInfo && herosInfo.length > 0">
+            <ul class="heroItem" v-for="(item, index) in herosInfo" :key="index" @click="showHeroInfo(item.name)">
                 <li>
                     <img :src="`${apiUrl}${item.name}.jpg`" alt="" class="heroImg">
                 </li>
@@ -13,43 +13,51 @@
                 </li>
             </ul>
         </div>
+        <div class="no-hero" v-else>暂无该式神</div>
     </div>
 </template>
 
 <script>
-    import vue from 'vue';
-    import ComHead from './comHead.vue';
-    export default {
-        name: 'heroList',
-        data() {
-            return {
-                title: '搜索结果',
-                herosInfo: null,
-                apiUrl: ''
-            };
-        },
-        components: {
-            ComHead
-        },
-        created() {
-            this.apiUrl = window.apiUrl;
-            vue.axios.get(`${window.apiUrl}hero/${this.$route.params[0]}`).then((response) => {
-                this.herosInfo = response.data;
-            });
-        },
-        methods: {
-            showHeroInfo(name) {
-                this.$router.push(`/hero/${name}`);
-            }
+import vue from 'vue';
+import ComHead from './comHead.vue';
+export default {
+    name: 'heroList',
+    data() {
+        return {
+            title: '搜索结果',
+            herosInfo: null,
+            apiUrl: ''
+        };
+    },
+    components: {
+        ComHead
+    },
+    created() {
+        this.apiUrl = window.apiUrl;
+        vue.axios.get(`${window.apiUrl}hero/${this.$route.params[0]}`).then((response) => {
+            this.herosInfo = response.data;
+        });
+    },
+    methods: {
+        showHeroInfo(name) {
+            this.$router.push(`/hero/${name}`);
         }
+    }
 
-    };
+};
 
 </script>
 
 <style lang="less">
-    @import '../util.less';
-    .searchHero> {
-        .px2rem(height, 1260);
-    }
+@import '../util.less';
+.searchHero {
+    .px2rem(height, 1260);
+}
+
+.no-hero {
+    .px2rem(padding-top, 100);
+    .px2rem(font-size, 16);
+    text-align: center;
+    color: #969696;
+}
 </style>
